d8dad387024302deb63bb2e6bca5dfc15f71e913
Ian <ra422983@uaeh.edu.mx>
2026-05-05 22:56:38 -0600

    Correccion

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const Producto = require('./apimongo');
+const Producto = require('./apiMongo');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
